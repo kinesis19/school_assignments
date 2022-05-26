3e445d58fc0017c3bad39ad4f66dfdbf23d63c6c
Kinesis <90908098+kinesis19@users.noreply.github.com>
2022-05-26 15:53:13 +0900

    1차 완성
    '정렬 전 데이터' 출력까지 구현 완료.
    
    앞으로 해야 하는 것
    1. 코드 리펙토링(배열 사용해서 길이 압축)
    2. (가능하면) '정렬 후 데이터' 까지 구현하기/-.-/

--- main.c
+++ main.c
@@ -6,13 +6,13 @@ char score_text(float avg);
 
 struct student{
 	int stu_num;
-	char stu_name;
+	char stu_name[10];
 	int kor; 
 	int eng;
 	int math;
 	int sum;
 	float avg;
-	char rating;
+	char rating[10];
 	
 };
 //학생 구조체 변수 생성 
@@ -69,34 +69,33 @@ int main(){
 		if(i == 1){
 			s1.sum = score_sum(s1.kor, s1.eng, s1.math);
 			s1.avg = score_avg(s1.kor, s1.eng, s1.math);
-			score_text(s1.avg);
+			s1.rating[0] = score_text(s1.avg);
 		}else if(i == 2){
 			s2.sum = score_sum(s2.kor, s2.eng, s2.math);
 			s2.avg = score_avg(s2.kor, s2.eng, s2.math);
+			s2.rating[0] = score_text(s2.avg);
 		}else if(i == 3){
 			s3.sum = score_sum(s3.kor, s3.eng, s3.math);
 			s3.avg = score_avg(s3.kor, s3.eng, s3.math);
+			s3.rating[0] = score_text(s3.avg);
 		}else if(i == 4){
 			s4.sum = score_sum(s4.kor, s4.eng, s4.math);
 			s4.avg = score_avg(s4.kor, s4.eng, s4.math);
+			s4.rating[0] = score_text(s4.avg);
 		}else if(i == 5){
 			s5.sum = score_sum(s5.kor, s5.eng, s5.math);
 			s5.avg = score_avg(s5.kor, s5.eng, s5.math);
+			s5.rating[0] = score_text(s5.avg);
 		}
 	}
 	//1차 출력 
 	printf("#정렬 전 데이터...\n");
 	
-	printf("시");
-	
-	printf("%d %s\n", s1.stu_num, s1.stu_name);
-	
-	//
-	printf("%d %s %d %d %d %d %.1f %c\n", s1.stu_num, s1.stu_name, s1.kor, s1.eng, s1.math, s1.sum, s1.avg, s1.rating);
-	printf("%d %s %d %d %d %d %.1f %c\n", s2.stu_num, s2.stu_name, s2.kor, s2.eng, s2.math, s2.sum, s2.avg, s2.rating);
-	printf("%d %s %d %d %d %d %.1f %c\n", s3.stu_num, s3.stu_name, s3.kor, s3.eng, s3.math, s3.sum, s3.avg, s3.rating);
-	printf("%d %s %d %d %d %d %.1f %c\n", s4.stu_num, s4.stu_name, s4.kor, s4.eng, s4.math, s4.sum, s4.avg, s4.rating);
-	printf("%d %s %d %d %d %d %.1f %c\n", s5.stu_num, s5.stu_name, s5.kor, s5.eng, s5.math, s5.sum, s5.avg, s5.rating);
+	printf("%d %s %d %d %d %d %.1f %c\n", s1.stu_num, s1.stu_name, s1.kor, s1.eng, s1.math, s1.sum, s1.avg, s1.rating[0]);
+	printf("%d %s %d %d %d %d %.1f %c\n", s2.stu_num, s2.stu_name, s2.kor, s2.eng, s2.math, s2.sum, s2.avg, s2.rating[0]);
+	printf("%d %s %d %d %d %d %.1f %c\n", s3.stu_num, s3.stu_name, s3.kor, s3.eng, s3.math, s3.sum, s3.avg, s3.rating[0]);
+	printf("%d %s %d %d %d %d %.1f %c\n", s4.stu_num, s4.stu_name, s4.kor, s4.eng, s4.math, s4.sum, s4.avg, s4.rating[0]);
+	printf("%d %s %d %d %d %d %.1f %c\n", s5.stu_num, s5.stu_name, s5.kor, s5.eng, s5.math, s5.sum, s5.avg, s5.rating[0]);
 	
 	return 0;
 }
@@ -108,21 +107,14 @@ float score_avg(int kor, int eng, int math){
 	return (float)(kor + eng + math) / 3;
 }
 char score_text(float avg){
-	int temp_score;
-	temp_score = (int)avg;
-	switch(temp_score/10){
-		case 9:
-			return 'A';
-			break;
-		case 8:
-			return 'B';
-			break;
-		case 7:
-			return 'C';
-			break;
-		default : 
-			return 'F';
-			break;
+	if(avg>=90){
+		return 'A';
+	}else if(avg>=80){
+		return 'B';
+	}else if(avg>=70){
+		return 'C';
+	}else{
+		return 'F';
 	}
 }
 
